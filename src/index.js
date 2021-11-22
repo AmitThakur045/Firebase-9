f0e347e2697eddf743d4841c5f7be75681b60baf
Amit <at7129652@gmail.com>
2021-11-22 19:25:39 +0530

    setting up firebase 9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello form index.js');
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbl-Bao0ZCb5Z_vUl09eONAiU5nY-P6xo",
+    authDomain: "fir-9-6e433.firebaseapp.com",
+    projectId: "fir-9-6e433",
+    storageBucket: "fir-9-6e433.appspot.com",
+    messagingSenderId: "742632653748",
+    appId: "1:742632653748:web:5bdedea2af679b7c5728e1"
+};
+
+initializeApp(firebaseConfig)
